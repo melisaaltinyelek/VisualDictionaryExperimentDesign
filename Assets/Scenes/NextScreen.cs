902000daf08e50ef3b70dc9e541992ac40e067d5
Melisa Altinyelek <melisaaltinyelek@melisas-air.localdomain>
2023-02-14 18:37:00 +0100

    added a script to switch between scenes

--- Assets/Scenes/NextScreen.cs
+++ Assets/Scenes/NextScreen.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
