fa5df017c833094c39d3f0327d1a041b2d4119d5
Melisa Altinyelek <melisaaltinyelek@83adcc8c.funky.uni-osnabrueck.de>
2023-02-22 14:36:00 +0100

    rt capture added

--- Assets/Scenes/VideoSwitcher.cs
+++ Assets/Scenes/VideoSwitcher.cs
@@ -5,12 +5,24 @@ using UnityEngine.Video;
 
 public class VideoSwitcher: MonoBehaviour
 {
+
+    private float startTime=0;
+    private float hazardAppearanceTime = 7f;
+    private bool hazardActive = false;
+
+
+
     public GameObject[] videos;
     private int currentVideoIndex;
 
     void Start()
     {
+        startTime = Time.time;
+        Time.timeScale = 1;
+        Debug.Log("Start method called");
+
         ShowRandomVideo();
+        
     }
 
     void Update()
@@ -19,6 +31,35 @@ public class VideoSwitcher: MonoBehaviour
         {
             ShowRandomVideo();
         }
+
+        if (Time.time >= hazardAppearanceTime && !hazardActive)
+        {
+            
+            hazardActive = true;
+            //Debug.Log("startTime: " + startTime);
+            Debug.Log("Hazard active");
+        }
+        if (hazardActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            startTime = Time.time;
+            Debug.Log("startTime: " + startTime);
+        }
+          if (startTime == 0)
+        {
+            startTime = Time.time;
+        }
+    
+        if (startTime > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            float reactionTime = Time.time - hazardAppearanceTime;
+            Debug.Log("Reaction time: " + reactionTime + " seconds");
+            Debug.Log("Total time: " + Time.time + " seconds");
+
+            // Write the reaction time to the file
+            //File.AppendAllText("reaction_times.csv", reactionTime + "\n");
+            startTime = 0;
+        }
+
     }
 
     void ShowRandomVideo()
