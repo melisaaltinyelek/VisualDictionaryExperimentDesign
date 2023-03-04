3b142201d6f0ca2a062e15ba4ce6dfe2e8e974d8
Gaiacoon <ggragnolati@uni-osnabrueck.de>
2023-03-04 14:04:56 +0100

    improved GUI's position and size, attached the instructions and welcome screen to the scenes index so now everything is playing before the actual hazard experiment and also added a end screen

--- Assets/Scenes/ParticipantID.cs
+++ Assets/Scenes/ParticipantID.cs
@@ -10,29 +10,45 @@ public class ParticipantID : MonoBehaviour
     private string participantID = "";
 
     private void OnGUI()
-    {
-        // Create a label and a text field for the participant ID
-        GUILayout.Label("Participant ID:");
-        participantID = GUILayout.TextField(participantID, GUILayout.Width(200));
+{
+    // Create a new GUIStyle
+    GUIStyle style = new GUIStyle(GUI.skin.label);
+    style.alignment = TextAnchor.MiddleCenter;
+    style.fontSize = 30;
 
-        // Create a button that saves the participant ID to a CSV file when clicked
-        if (GUILayout.Button("Save ID", GUILayout.Width(100)))
-        {
-            SaveParticipantID();
-            
-        }
+    // Create a label and a text field for the participant ID
+    GUI.Label(new Rect(0, Screen.height / 2 - 75, Screen.width, 50), "Participant ID:", style);
+
+    // Create a GUIStyle for the text field with a larger font size
+    GUIStyle textFieldStyle = new GUIStyle(GUI.skin.textField);
+    textFieldStyle.fontSize = 30;
+
+    participantID = GUI.TextField(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 25, 300, 40), participantID, textFieldStyle);
+
+    // Create a button that saves the participant ID to a CSV file when clicked
+    GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+    buttonStyle.fontSize = 25;
+    buttonStyle.fixedWidth = 150;
+    buttonStyle.fixedHeight = 40;
+
+    if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 + 50, 150, 40), "Save ID", buttonStyle))
+    {
+        SaveParticipantID();
     }
+}
+
+
 
     private void SaveParticipantID()
     {
         // Append the participant ID to a CSV file
-        string filePath = Path.Combine(Application.dataPath, "reaction_times.csv");
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
         StreamWriter writer = new StreamWriter(filePath, true);
         writer.WriteLine(participantID);
         writer.Close();
-
 
         // Load the next scene (we can put name of the scene e.g. "welcome screen")
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
 }
+
