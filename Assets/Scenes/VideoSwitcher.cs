7399d21cb7f90302e06671a39a85a1f687733019
Gaiacoon <ggragnolati@uni-osnabrueck.de>
2023-03-04 13:01:51 +0100

    put scenes in order

--- Assets/Scenes/VideoSwitcher.cs
+++ Assets/Scenes/VideoSwitcher.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 using System.IO;
 
 public class VideoSwitcher : MonoBehaviour
